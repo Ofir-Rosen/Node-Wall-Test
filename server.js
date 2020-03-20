a12c537bf415c9f63eb3580aff466c97d3a7cadd
Ofir-Rosen <48698558+Ofir-Rosen@users.noreply.github.com>
2020-03-20 11:34:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 var express = require ('express');
 //require express, store it in app, and then use app to listen on port 3000
 var app = express();
-var server = app.listen(5000);
-
+//var server = app.listen(5000);
+var server = app.listen(process.env.PORT || 3000)
 //host the files in the public folder on this server on port 3000
 app.use(express.static('public'));
 //static means the files are not changing
